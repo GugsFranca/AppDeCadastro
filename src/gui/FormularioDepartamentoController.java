a1c3b2479fb13e1f380004160c891e7a94590a12
Gustavo de França <gustavonota20@hotmail.com>
2023-09-20 12:13:37 -0300

    Adicionando uma validação mais completa ao campo nome do formulario

--- src/gui/FormularioDepartamentoController.java
+++ src/gui/FormularioDepartamentoController.java
@@ -13,12 +13,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartamentoService;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class FormularioDepartamentoController implements Initializable {
 
@@ -41,11 +40,20 @@ public class FormularioDepartamentoController implements Initializable {
 
     private Department getFormData() {//pega os dados do formulario
         Department obj = new Department();
+        ValidationException exception = new ValidationException("Erro de Validação");
         obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+        if (txtNome.getText() == null || txtNome.getText().trim().isEmpty()) {
+            exception.addError("nome", "O campo nome não pode ser vazio ");
+        }
         obj.setNome(txtNome.getText());
+        if (exception.getErrors().size() > 0) {
+            throw exception;
+        }
 
         return obj;
     }
+
     public void setDepartment(Department entity) {
         this.entity = entity;
     }
@@ -54,12 +62,12 @@ public class FormularioDepartamentoController implements Initializable {
         this.service = service;
     }
 
-    public void subscribeDataChangeListener(DataChangeListener listener){
+    public void subscribeDataChangeListener(DataChangeListener listener) {
         dataChangeListeners.add(listener);
     }
 
     private void notifyDatachangeListeners() {
-        for(DataChangeListener listener : dataChangeListeners){
+        for (DataChangeListener listener : dataChangeListeners) {
             listener.onDataChange();
         }
     }
@@ -77,6 +85,8 @@ public class FormularioDepartamentoController implements Initializable {
             service.saveOrUpdate(entity);
             notifyDatachangeListeners();
             Utils.currentStage(event).close();
+        } catch (ValidationException e) {
+            setErrorMessages(e.getErrors());
         } catch (DbException e) {
             Alerts.showAlert("Erro ao salvar objeto", null, e.getMessage(), Alert.AlertType.ERROR);
         }
@@ -103,5 +113,12 @@ public class FormularioDepartamentoController implements Initializable {
         }
         txtId.setText(String.valueOf(entity.getId()));
         txtNome.setText(entity.getNome());
+    }
+
+    private void setErrorMessages(Map<String, String> error) {
+        Set<String> fields = error.keySet();
+        if (fields.contains("nome")) {
+            labelErrorName.setText(error.get("nome"));
+        }
     }
 }
